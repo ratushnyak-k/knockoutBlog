6ddce8009e6080e58879ef9dd371ca0d6eb7d6ee
Konstantin Ratushnyak <ratushnyak@steelkiwi.com>
2016-01-04 17:18:05 +0200

    Create/remake comments viewmodel for each article

--- src/js/partials/scripts.js
+++ src/js/partials/scripts.js
@@ -61,35 +61,43 @@ function ViewModel () {
                 'img/1p.jpg',
                 Ivan.fullName(),
                 Ivan.ava(),
-                (new Date().getTime() + 100000), 0, 0, 4),
+                (new Date().getTime() + 100000), 0, 0, 4,
+                [
+                    new Comment(myInfo.fullName(), myInfo.ava(), 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, officiis?', new Date()),
+                    new Comment(Ivan.fullName(), Ivan.ava(), 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, officiis?', new Date()),
+                    new Comment(Dmitry.fullName(), Dmitry.ava(), 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, officiis?', new Date()),
+                    new Comment(Ruslan.fullName(), Ruslan.ava(), 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, officiis?', new Date()),
+                    new Comment(Artem.fullName(), Artem.ava(), 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, officiis?', new Date()),
+                    new Comment(Alex.fullName(), Alex.ava(), 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, officiis?', new Date()),
+                ]),
             new PostsList(
                 'Second',
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident fugit alias quidem ea facere, deleniti quam ipsam vel suscipit optio aliquam minima aspernatur repellendus distinctio accusamus dolores tempore accusantium. Impedi lorem  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum nisi, facilis, suscipit ipsum quaerat adipisci inventore eveniet provident nesciunt dolorem, blanditiis voluptatem asperiores? Velit sequi, doloribus, et aut quod ducimus Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque repudiandae eveniet in ipsum sequi quia minus nesciunt aliquid, non, laudantium nam eum quo necessitatibus reprehenderit obcaecati sapiente rem sunt excepturi, ex dolore quibusdam nemo earum odit et. Placeat delectus itaque natus voluptas, omnis ad nostrum, rem perferendis eos laborum cum, incidunt autem voluptatem commodi dignissimos. Totam odio, quidem quasi ratione est eum nesciunt at ullam similique reprehenderit! Pariatur distinctio repudiandae quidem minima dolore possimus commodi, veniam quos cupiditate ducimus rerum qui fugiat labore perspiciatis nemo itaque aliquam ea enim, amet consequatur eos obcaecati. Et recusandae, ipsam unde debitis ipsa perferendis odit provident accusantium necessitatibus, similique ducimus doloribus maxime, incidunt laudantium modi! Cum quos inventore alias ut quam tenetur magni pariatur. Soluta beatae quod voluptate animi ipsa impedit! Dolorem vel error, possimus nihil quia, obcaecati, itaque illo sit, beatae quaerat perspiciatis quis nesciunt velit distinctio repudiandae sequi nisi. Recusandae facere asperiores fuga sapiente tempora voluptates perspiciatis eius praesentium porro, unde maxime veniam minima cupiditate enim nisi explicabo beatae deserunt eum laboriosam saepe illo vel repellendus assumenda corporis. Animi nemo impedit maiores quisquam velit neque excepturi corporis quae enim earum quo laborum molestias, harum suscipit debitis dolore necessitatibus quasi. Facilis quis, quas.',
                 'img/2p.jpg',
                 Dmitry.fullName(),
                 Dmitry.ava(),
-                (new Date().getTime() + 50000), 24, 0, 7),
+                (new Date().getTime() + 50000), 24, 0, 7, []),
             new PostsList(
                 'Third',
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, similique. Beatae, nam, quia! Nobis quo, iste repudiandae atque labore at sunt! Dolore iste, alias laboriosam nihil, nesciunt quam assumenda ame lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eum consequuntur ut, impedit sequi harum necessitatibus, quos culpa vero! Harum magnam ex quam eum nihil quis iste rerum laboriosam recusandae Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo omnis, quidem ad quia at ullam iure ratione quod ducimus reiciendis fugiat laboriosam doloremque, expedita amet rem esse architecto deleniti asperiores repudiandae cumque officia voluptatibus non. Quasi maxime, earum quos repudiandae pariatur cupiditate sapiente id consectetur mollitia fuga provident illum esse nemo voluptates similique labore alias, quas officiis temporibus veniam dolorum maiores quisquam perspiciatis! Magni ad culpa id veniam, voluptas velit praesentium nemo minima excepturi, nobis, doloremque quos! Culpa earum facere cupiditate nesciunt quis consequuntur possimus recusandae necessitatibus. Officia qui aperiam, maiores veniam ab laborum velit voluptatem, asperiores provident excepturi temporibus tempore perspiciatis reprehenderit, sunt! Quae veniam excepturi placeat expedita facilis natus. Labore laudantium porro molestiae cumque est officiis vero dolorum ipsa, quibusdam nulla eveniet a molestias aut, cupiditate nam debitis modi in distinctio, voluptate illum velit! Facilis eaque laborum, assumenda voluptatibus quod maxime nam voluptas, reprehenderit rerum debitis ipsa quam excepturi ipsam unde perferendis. Sit sequi quidem at magni, assumenda odit impedit atque consequuntur nostrum mollitia ipsam, vel pariatur modi unde asperiores iure omnis nobis! Laboriosam officiis quae possimus repellat est maiores provident iste facere commodi error aperiam iusto soluta voluptatem perferendis ea nisi, unde quia cupiditate quas fuga vel.',
                 'img/3p.jpg',
                 Ruslan.fullName(),
                 Ruslan.ava(),
-                new Date().getTime(), 144, 0, 35),
+                new Date().getTime(), 144, 0, 35, []),
             new PostsList(
                 'Fourth',
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dolorem cupiditate tenetur vitae molestias odit, fugit dignissimos pariatur. Beatae, nisi. Nemo nobis cupiditate dolore ratione, quidem sint incidunt at consequuntur lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam est quos, quas! Quaerat suscipit optio delectus, voluptatem itaque reprehenderit ipsa impedit eos, rem similique laborum possimus maiores esse minus. Pariatur Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo nihil eaque nobis officia eius illum possimus nulla, ad adipisci doloribus cupiditate blanditiis placeat, totam sit. Porro, illo repellat, minus dolore fugit laborum ad officiis optio error. Laborum praesentium at esse necessitatibus explicabo adipisci, quibusdam cumque assumenda optio animi sed ratione, harum accusantium culpa eos labore, ipsam omnis aliquid voluptatem. Est officia reiciendis aliquid, exercitationem repellendus, voluptatum autem hic beatae nemo sit eveniet placeat esse quos commodi incidunt voluptatem doloribus. Voluptates iusto inventore, rem ipsa blanditiis totam temporibus sint itaque at consectetur velit reiciendis voluptate doloribus laborum exercitationem dignissimos doloremque laudantium atque distinctio consequatur. Debitis, maxime optio eveniet minima aliquid sit ipsam tempore adipisci dicta ducimus sint delectus, praesentium libero, incidunt quas! Dicta reiciendis sunt incidunt enim nulla veritatis nemo tempore harum nisi eum quis tempora quo, asperiores nobis architecto, aut voluptates vitae quidem doloribus illo voluptatibus similique! Cum odit minus sunt sequi officiis quae, iste provident. Excepturi magni iure molestias amet eveniet et repellendus earum voluptas. Accusantium magni minus sapiente dolores in quisquam quia neque pariatur animi officiis. Aspernatur amet nam eos in. Delectus itaque quos error reiciendis veritatis distinctio doloremque, ipsa nulla labore nobis, maiores amet, repellendus beatae dignissimos.',
                 'img/4p.jpg',
                 Artem.fullName(),
                 Artem.ava(),
-                new Date().getTime(), 154, 0, 134),
+                new Date().getTime(), 154, 0, 134, []),
             new PostsList(
                 'Fifth',
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque harum illum rerum nisi perspiciatis, quos repudiandae qui iste assumenda sed ducimus incidunt et ullam excepturi at consequatur ab inventore dolor lorem   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid porro, maxime omnis possimus dolore in, debitis, numquam minus doloribus voluptates optio ratione accusantium accusamus voluptas fuga perferendis corporis quia autem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit natus corrupti ex dolore, nemo nesciunt quae nisi. Modi perferendis enim aperiam dolorem perspiciatis reiciendis laboriosam quis assumenda, consequatur et repellendus accusamus commodi minus, ab fugiat ea sint voluptas voluptatem praesentium nihil deserunt temporibus ipsa excepturi illo? Officiis veniam sint recusandae culpa odit iste, in! Provident natus repellat harum eum beatae, porro. Vitae facere repudiandae libero quam mollitia minus, odit, amet in recusandae itaque nostrum. Provident officia earum quo quidem, praesentium magni. Optio voluptas cupiditate quasi sapiente cum natus, suscipit aliquid aliquam dignissimos officiis eaque, omnis repellendus veniam dolore esse, facere blanditiis eum quis. Accusamus, eius deleniti doloribus quis temporibus quidem id tempore aspernatur laborum est blanditiis sed fugit alias culpa exercitationem totam. Nemo doloribus deleniti placeat aliquam unde consequuntur veniam dignissimos consectetur eveniet vel tempora nulla obcaecati, quam facilis. Deserunt quo eveniet, distinctio non ullam tempore dicta doloremque minus sunt adipisci sequi vel voluptatibus numquam architecto alias repellat reiciendis mollitia placeat temporibus ea pariatur quibusdam. Veniam illo reprehenderit in maxime inventore quae repellendus laboriosam ut quas, dolorum voluptate amet placeat, commodi reiciendis corporis libero tempore distinctio officia, excepturi sed assumenda! Expedita vero harum quasi fugiat! Itaque alias eligendi, delectus dolorem.',
                 'img/5p.jpg',
                 Alex.fullName(),
                 Alex.ava(),
-                new Date().getTime(), 14, 0, 4)
+                new Date().getTime(), 14, 0, 4, [])
         ]
     );
     self.authorIsVisible = function () {
@@ -124,7 +132,7 @@ function ViewModel () {
     }, this);
 
     self.addPost = function () {
-        self.posts.unshift(new PostsList(self.title(), self.text(), self.img(), myInfo.fullName(), myInfo.ava(), new Date().getTime(), 0, 0, 0));
+        self.posts.unshift(new PostsList(self.title(), self.text(), self.img(), myInfo.fullName(), myInfo.ava(), new Date().getTime(), 0, 0, 0, []));
         self.readVisiblity(true);
     };
     self.queryPost = ko.observable('');
@@ -138,21 +146,11 @@ function ViewModel () {
         }
     });
 
-    self.commentsArray = ko.observableArray(
-        [
-            new Comment(myInfo.fullName(), myInfo.ava(), 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, officiis?', new Date()),
-            new Comment(Ivan.fullName(), Ivan.ava(), 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, officiis?', new Date()),
-            new Comment(Dmitry.fullName(), Dmitry.ava(), 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, officiis?', new Date()),
-            new Comment(Ruslan.fullName(), Ruslan.ava(), 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, officiis?', new Date()),
-            new Comment(Artem.fullName(), Artem.ava(), 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, officiis?', new Date()),
-            new Comment(Alex.fullName(), Alex.ava(), 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, officiis?', new Date()),
-        ]
-    );
     self.newCommentText = ko.observable('');
     self.editedCommentText = ko.observable('');
 
     self.addComment = function () {
-        self.commentsArray.unshift(new Comment(myInfo.fullName(), myInfo.ava(), self.newCommentText(), new Date()));
+        this.commentsArray.unshift(new Comment(myInfo.fullName(), myInfo.ava(), self.newCommentText(), new Date()));
         self.newCommentText('');
     };
 
@@ -167,11 +165,11 @@ function ViewModel () {
     };
 
     self.deleteComment = function () {
-        self.commentsArray.remove(this);
+        self.posts()[thisHash].commentsArray.remove(this);
     };
 }
 
-function PostsList (title, text, img, author, ava, timestamp, totalRate, rate, countPeopleRate) {
+function PostsList (title, text, img, author, ava, timestamp, totalRate, rate, countPeopleRate, CommentsArray) {
     var self = this;
     self.post_header = title;
     self.post_main = text;
@@ -191,6 +189,7 @@ function PostsList (title, text, img, author, ava, timestamp, totalRate, rate, c
     self.totalRate = ko.observable(proccessingRate);
     self.rate = ko.observable(rate);
     self.ratingSetted = ko.observable(false);
+    self.commentsArray = ko.observableArray(CommentsArray);
 }
 function readURL(input, whatModel) {
 
@@ -241,7 +240,6 @@ ko.bindingHandlers.validation = {
                     if(firstNameRegEx.test(bindingContext.firstName())) {
                         errorValidate(element, '');
                         firstNameField = true;
-
                     } else {
                         errorValidate(element, 'This is not a name');
                     }
@@ -322,17 +320,3 @@ var usersList = new Users();
 if (document.getElementById('users')) {
     ko.applyBindings(usersList, document.getElementById('users'));
 }
-
-// function Comments () {
-//     var self = this;
-
-
-// }
-
-
-
-// var commentsModel = new Comments();
-
-// if ($('#comments').length) {
-//     ko.applyBindings(commentsModel, document.getElementById('comments'))
-// }
